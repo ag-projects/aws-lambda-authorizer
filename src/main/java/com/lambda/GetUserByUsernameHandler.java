f82e87ce8a5ed30a352eca1e6cd3c9f19ddc224e
Armen-02 <armen.aws@protonmail.com>
2023-06-10 18:33:18 -0700

    Added base classes

---
+++ src/main/java/com/lambda/GetUserByUsernameHandler.java
@@ -0,0 +1,14 @@
+package com.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class GetUserByUsernameHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+        return new APIGatewayProxyResponseEvent();
+    }
+}
