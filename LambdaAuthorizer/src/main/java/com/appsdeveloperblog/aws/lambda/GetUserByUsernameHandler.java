a4ff71bdf2fdc29d5c16b16f6191a531af1e49b6
Armen-02 <armen.aws@protonmail.com>
2023-06-17 08:38:24 -0700

    Add user User detials

--- LambdaAuthorizer/src/main/java/com/appsdeveloperblog/aws/lambda/GetUserByUsernameHandler.java
+++ LambdaAuthorizer/src/main/java/com/appsdeveloperblog/aws/lambda/GetUserByUsernameHandler.java
@@ -4,10 +4,44 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.appsdeveloperblog.aws.lambda.service.CognitoUserService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GetUserByUsernameHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private final CognitoUserService cognitoUserService;
+
+    public GetUserByUsernameHandler() {
+        this.cognitoUserService = new CognitoUserService("AWS_REGION");
+    }
+
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
-        return new APIGatewayProxyResponseEvent();
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        Map<String, String> header = new HashMap<>();
+        header.put("Content-Type", "application/json");
+        response.setHeaders(header);
+
+        String userName = input.getPathParameters().get("userName");
+        String poolId = System.getenv("PHOTO_APP_USERS_POOL_ID");
+
+        JsonObject userDetails;
+        try {
+            userDetails = cognitoUserService.getUserByUsername(userName, poolId);
+            response.withBody(new Gson().toJson(userDetails, JsonObject.class));
+            response.withStatusCode(200);
+        } catch (Exception e) {
+            response.setBody("{\n" +
+                    "\t\"message\":\"e.exception\"\n" +
+                    "}");
+            response.withStatusCode(500);
+            return response;
+        }
+
+        return response;
     }
 }
--- LambdaAuthorizer/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
+++ LambdaAuthorizer/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
@@ -1,9 +1,23 @@
 package com.appsdeveloperblog.aws.lambda.service;
 
 import com.google.gson.JsonObject;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminGetUserRequest;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminGetUserResponse;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeType;
+
+import java.util.List;
 
 public class CognitoUserService {
+
+    private final CognitoIdentityProviderClient cognitoIdentityProviderClient;
+
+    public CognitoUserService(String region) {
+        this.cognitoIdentityProviderClient = CognitoIdentityProviderClient.builder()
+                .region(Region.of(region))
+                .build();
+    }
 
     public JsonObject getUserByUsername(String userName, String poolId) {
         AdminGetUserRequest  adminGetUserRequest = AdminGetUserRequest.builder()
@@ -11,6 +25,14 @@ public class CognitoUserService {
                 .userPoolId(poolId)
                 .build();
 
-        return null;
+        AdminGetUserResponse adminGetUserResponse = cognitoIdentityProviderClient.adminGetUser(adminGetUserRequest);
+        JsonObject userDetails = new JsonObject();
+        if (!adminGetUserResponse.sdkHttpResponse().isSuccessful()) {
+            throw new RuntimeException("Unsuccessful result. Status code: " + adminGetUserResponse.sdkHttpResponse().statusCode());
+        }
+        List<AttributeType> attributes = adminGetUserResponse.userAttributes();
+        attributes.forEach(attribute -> userDetails.addProperty(attribute.name(), attribute.value()));
+
+        return userDetails;
     }
 }
